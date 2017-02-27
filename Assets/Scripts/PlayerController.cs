2b233ad5f10f0a7cb84a078846b6064da61cceaa
Martin Hansen <Martin Hansen>
2017-02-27 15:24:07 +0100

    added player playObject, playerController script added with movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+
+	private Rigidbody rb;
+	public float moveSpeed;
+
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 direction;
+		if (horizontal != 0f) {
+			direction = new Vector3 (horizontal, 0f, 0f);
+			rb.velocity = direction * moveSpeed * Time.deltaTime;
+		} else if (vertical != 0f) {
+			direction = new Vector3 (0f, 0f, vertical);
+			rb.velocity = direction * moveSpeed * Time.deltaTime;
+		}
+
+	}	
+}
