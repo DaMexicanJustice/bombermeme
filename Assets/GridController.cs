5df7bb35151e910fbb749bf9ac54ca6e53150b87
Martin Hansen <Martin Hansen>
2017-03-02 11:36:23 +0100

    Added Grid with a script to contain objects(bombs, boxes), Added bomb prefab and the ability to spawn bombs in the player script - started on the part where we destroy blocks with bombs

---
+++ Assets/GridController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour {
+
+
+	public GameObject box;
+	private Quaternion quat;
+	private Vector3 spawnPosition = new Vector3(14,1,-6.5f);
+	private Vector3 addingToX = new Vector3(1,0,0);
+	private Vector3 addingToZ = new Vector3(-9,0,1);
+	private int tempInt = 0;
+
+
+	GameObject[,] grid = new GameObject[10,13];
+
+	void Start(){
+		
+		for (int i = 0; i <= 10; i++) {
+
+			if (Random.Range (0, 2) == 1) {
+				grid [i, i] = (GameObject)Instantiate (box, spawnPosition, quat);
+			}
+
+			spawnPosition = spawnPosition + addingToX;
+			if (i == 9) {
+				tempInt++;
+				i = 0;
+				spawnPosition = spawnPosition + addingToZ;
+				if (tempInt > 13) {
+					return;
+				}
+			}
+	}
+}
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 
 	private Rigidbody rb;
 	public float moveSpeed;
-
+	public GameObject bombPrefab;
+	public GameObject grid;
+	public GameObject other;
 
 	void Start () {
 
 		rb = GetComponent<Rigidbody> ();
 
+	}
+
+	void PlantBomb(){
+
+		Vector3 roundPos = rb.position;
+		roundPos = new Vector3(Mathf.Round(roundPos.x),Mathf.Round(roundPos.y),Mathf.Round(roundPos.z));
+
+
+		var bomb = (GameObject)Instantiate (bombPrefab, roundPos, rb.rotation);
+			
+		Destroy (bomb, 4.0f);
+
+//		Måske et Array over dem alle?
+		Vector3 north = new Vector3 (roundPos.x, roundPos.y, roundPos.z+1);
+		Vector3 south = new Vector3 (roundPos.x, roundPos.y, roundPos.z-1);
+		Vector3 west = new Vector3 (roundPos.x-1, roundPos.y, roundPos.z);
+		Vector3 east = new Vector3 (roundPos.x+1, roundPos.y, roundPos.z);
+
+		if(Physics.CheckSphere(north,1)){
+		//	Destroy (,4.0f);
+		}	
 	}
 	
 	// Update is called once per frame
@@ -23,6 +47,10 @@ public class PlayerController : MonoBehaviour {
 		float vertical = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
         rb.velocity = direction * moveSpeed * Time.deltaTime;
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			PlantBomb();
+		}
 
         /*
 		if (horizontal != 0f) {
