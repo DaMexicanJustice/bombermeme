6e1cd8d02083481db94c6e30d716a689d9e8ba18
Patrick Johansen <Patrick Johansen>
2017-03-06 16:57:35 +0100

    Added powerup cube with rotation script

---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		transform.Rotate (new Vector3 (0,45,0) * rotationSpeed * Time.deltaTime);
+	}
+}
