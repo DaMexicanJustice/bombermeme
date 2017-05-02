3b32fc33d4d0c2894807ef46597d0fc436e2a275
Patrick Johansen <Patrick Johansen>
2017-05-02 11:26:52 +0200

    Implemented a canvas to handle all graphical events sent from Twitch and implemented Kappa event set to spawn on start for demo

---
+++ Assets/GameEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEvents : MonoBehaviour {
+
+	public GameObject canvas;
+	public GameObject kappa;
+
+	// Use this for initialization
+	void Start () {
+		StartKappaEvent ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartKappaEvent() {
+		Vector3 origin = new Vector3 (Screen.width / 2, Screen.height + 100 ,0);
+		GameObject k = Instantiate (kappa, origin, Quaternion.identity);
+		k.transform.parent = canvas.transform;
+	}
+}
